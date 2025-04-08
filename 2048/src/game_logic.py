2b59ba06bfc7ac9e3c513b864446a28843ef5f4f
Ville Davidsson <ville.d@hotmail.com>
2025-04-08 18:15:58 +0300

    expanded game logic to allow moving tiles and merging tiles of the same value

--- 2048/src/game_logic.py
+++ 2048/src/game_logic.py
@@ -18,11 +18,48 @@ class GameLogic:
         self.add_tile()
         self.add_tile()
 
-    def move_tiles(self, direction):
-        pass
+    def move_and_merge_tiles(self, grid: np.ndarray):
+        new_grid = np.zeros_like(grid)
+        for row in range(grid.shape[0]):
+            first_merge = self.merge_row(grid[row])
+            merged_row = self.merge_row(
+                np.array(first_merge + [0] * (len(grid[row]) - len(first_merge))))
+            new_grid[row] = np.array(
+                merged_row + [0] * (len(grid[row]) - len(merged_row)))
+        return new_grid
+
+    def merge_row(self, row):
+        skip = False
+        merged_row = []
+        compressed_row = row[row != 0]
+
+        for i in range(len(compressed_row)):
+            if skip:
+                skip = False
+                continue
+            else:
+                if i + 1 < len(compressed_row) and compressed_row[i] == compressed_row[i+1]:
+                    merged_row.append(compressed_row[i]*2)
+                    skip = True
+                else:
+                    merged_row.append(compressed_row[i])
+        return merged_row
 
     def update_grid(self, direction):
-        pass
+        match direction:
+            case 'left':
+                self.grid = self.move_and_merge_tiles(np.copy(self.grid))
+            case 'down':
+                new_grid = self.move_and_merge_tiles(np.rot90(self.grid, k=-1))
+                self.grid = np.rot90(new_grid, k=1)
+            case 'right':
+                new_grid = self.move_and_merge_tiles(np.rot90(self.grid, k=2))
+                self.grid = np.rot90(new_grid, k=-2)
+            case 'up':
+                new_grid = self.move_and_merge_tiles(np.rot90(self.grid, k=1))
+                self.grid = np.rot90(new_grid, k=-1)
+
+        self.add_tile()
 
     def __str__(self):
         return str(self.grid)
